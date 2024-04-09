768ac1ee3140d594ab998525ed186018a90881ca
André Furchner <storkner@gmail.com>
2024-04-09 23:37:22 +0200

    import all data

--- src/demo/keep-distance/input/import.ts
+++ src/demo/keep-distance/input/import.ts
@@ -3,17 +3,27 @@ export class Import {
     maxEdges = 0;
     maxForce = 0;
     index: number = 0;
+    items;
     map = {}
     indexToId = [];
 
+
+    settings: {
+        colors: any
+        maxEdges: number
+        nodes: number
+        data: any
+        Size: number
+        items: any
+    } = {} as any;
 
     constructor() {
 
     }
 
     async start() {
-        // const res = await fetch("/resources/items.json");
-        // this.map = await res.json();
+        const res = await fetch("/resources/items.json");
+        this.items = await res.json();
         await this.streamCSV("/resources/resultrules.csv");
     }
 
@@ -33,6 +43,7 @@ export class Import {
         // Read and process the CSV data line by line
         let decoder = new TextDecoder();
         let buffer = '';
+        let first = true;
 
         while (true) {
             const { done, value } = await stream.read();
@@ -40,15 +51,94 @@ export class Import {
 
             buffer += decoder.decode(value, { stream: true });
 
-            const lines = buffer.split('\n');
+            let lines = buffer.split('\n');
+
+            if (first) {
+                // ignore csv header
+                lines = lines.splice(1);
+                first = false;
+            }
 
             // Save the last incomplete line for the next iteration
             buffer = lines.pop() || '';
 
             // Process complete lines
             this.parseConnections(lines);
-            console.log(lines.length);
         }
+
+        const data = [];
+        const test = [];
+        let missingData = 0;
+        // TODO
+        let base = Math.sqrt(Object.keys(this.map).length);
+        if (base % 1 !== 0) {
+            base = this.nextPo2(base);
+        }
+
+        if (!this.isPo2(this.maxEdges)) {
+            this.maxEdges = this.nextPo2(this.maxEdges);
+        }
+
+        const colors = [];
+        const filteredItems = {};
+        for (const key in this.map) {
+            filteredItems[key] = this.items[key];
+
+            const edges = [];
+            for (const edge of this.map[key].edges) {
+                if (this.map[edge.conc]) { // check was needed for some data sets
+                    const index: number = this.map[edge.conc].id;
+                    let y = Math.floor(index / base);
+                    let x = index % base;
+                    edges.push([x, y, edge.force / this.maxForce, 0])
+                }
+            }
+
+            if (edges.length === 0) {
+                missingData++;
+            }
+
+            // padding
+            for (let p = edges.length; p < this.maxEdges; p++) {
+                edges.push([-1, -1, -1, 0]);
+            }
+            data[this.map[key].id] = edges;
+            this.indexToId[this.map[key].id] = key;
+        }
+
+        let buf = [];
+        let i = 0;
+        for (const node of data) {
+
+            const item = this.items[this.indexToId[i]];
+            if (item) {
+                colors.push(...this.fixDarkColors(item.color));
+            } else {
+                // not in items.json use default color
+                colors.push(...[255,255,255]);
+            }
+
+
+            for (const edge of node) {
+                buf.push(...edge);
+            }
+
+            i++;
+        }
+
+        console.log("Missing Data: ", missingData);
+        console.log("Nodes: ", Object.keys(this.map).length);
+        console.log("Size: ", base);
+        console.log("Max. edges: ", this.maxEdges);
+
+        colors.length = base * base * 3;
+
+        this.settings.colors = colors;
+        this.settings.maxEdges = this.maxEdges;
+        this.settings.nodes = Object.keys(this.map).length;
+        this.settings.data = buffer;
+        this.settings.Size = base;
+        this.settings.items = filteredItems;
     }
 
     private parseConnections(lines: string[]) {
@@ -56,10 +146,9 @@ export class Import {
             const v = lines[i].split("|");
 
             // filter missing items
-            // TODO?
-            // if (!items[v[0]] || !items[v[1]]) {
-            //     continue;
-            // }
+            if (!this.items[v[0]] || !this.items[v[1]]) {
+                continue;
+            }
 
             let prem;
             if (this.map[v[0]]) {
@@ -97,5 +186,15 @@ export class Import {
 
     private isPo2(v): boolean {
         return v && !(v & (v - 1));
+    }
+
+
+    private fixDarkColors(color: number[]) {
+        // TODO
+        // const c = vec3.fromValues(color[0], color[1], color[2]);
+        // if (vec3.length(c) < 128.0) {
+        //     return [96, 96, 96];
+        // }
+        return  color;
     }
 }
